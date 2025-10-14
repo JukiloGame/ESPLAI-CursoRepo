88e13823f7c2f84bfbec37d0aaf5dc682ade6c7c
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-10-14 15:24:26 +0200

    feat: explicación del ejercicio 03

--- JavaScript/sh01/ex03/main.js
+++ JavaScript/sh01/ex03/main.js
@@ -3,3 +3,9 @@ console.log("5" - 2);
 console.log(true + true);
 console.log(false == 0);
 console.log(10 === "10");
+
+// 1: Lo lee como String así que concatena los valores
+// 2: Lo lee como Number y realiza la resta
+// 3: true == 1; con lo cual true + true
+// 4: false evale a 0 (true y false son 1 y 0 respectivamente, igual que en binario)
+// 5: la expresión === hace referencia a que no solo coincida el valor, si no también su tipo (completamente igual)
