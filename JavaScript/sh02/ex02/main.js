fdb3254817ef03e9d614fd93df61a8a2b0729fb4
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-10-14 19:44:45 +0200

    feat: crear función maximoDe y testearla

--- JavaScript/sh02/ex02/main.js
+++ JavaScript/sh02/ex02/main.js
@@ -1,14 +1,22 @@
 function sumatorio(base = 0, ... nums) {
 	let total = base;
 	for (let num of nums) {
-		if(typeof num == "number") {
+		if(typeof num === "number") {
 			total+=num;
 		}
 	}
 	return total;
 }
 
+function maximoDe(lista = []) {
+	let higherNum = 0;
+	for (let num of lista) {
+		num > higherNum ? higherNum = num : higherNum;
+	}
+	return higherNum
+}
 
 console.log(sumatorio(10, 1, 2, '3', 4));
+console.log(maximoDe([1,9,3]));
 
 
