c7bb26319696835531730f4f16d979f486491382
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-10-14 15:33:18 +0200

    feat: ejecucion de condicionales explicados

---
+++ JavaScript/sh01/ex04/main.js
@@ -0,0 +1,15 @@
+if(false || "JavaScript"){
+	console.log("1 = true");
+}
+
+if(true && "ECMAScript"){
+	console.log("2 = true");
+}
+
+if(!0){
+	console.log("3 = true");
+}
+
+// 1: || significa "o", asi que es verdadero si alguno de los dos es verdadero
+// 2: && significa "y", asi que es verdadero si ambos son verdaderos
+// 3: ! significa "distinto de", asi que es verdadero si el valor es falso
