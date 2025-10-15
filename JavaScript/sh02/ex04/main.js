7107a5218ad024546b98958c1b12a57275a6b63a
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-10-15 17:04:40 +0200

    feat(ex04): crear memorize y hacer prueba rápida

--- JavaScript/sh02/ex04/main.js
+++ JavaScript/sh02/ex04/main.js
@@ -6,4 +6,28 @@ function createCounter(start, step) {
 	};
 }
 
+function memorize(fn) {
+	const cache = {};
+	return function (...args) {
+		const key = JSON.stringify(args)
+		if (cache[key]) {
+			return cache[key];
+		}
+		const result = fn(...args)
+		cache[key] = result;
+		return result;
+	}
+}
+
 const inc = createCounter(5,2); inc(); inc();
+
+const contarLlamadas = () => {
+	let n=0;
+	const f = (x)=>{ n++; return x*x; };
+	const llamadas = () => n;
+	return { f, llamadas };
+};
+const { f, llamadas } = contarLlamadas();
+const mf = memorize(f); mf(5); mf(5);
+
+console.log(llamadas()); // 1
