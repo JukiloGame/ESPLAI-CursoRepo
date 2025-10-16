2397d7e9246bd333699adc3aac829815475a9b32
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-10-16 15:13:00 +0200

    chore(ex01):  añadir salida en consola para variables del ejercicio

---
+++ JavaScript/sh03/ex01/main.js
@@ -0,0 +1,13 @@
+const a = [3,1,2];
+
+// Completa con comentarios:
+a.sort();
+const b = a.slice(0, 2);
+const c = b.toReversed();
+const d = a.with(0, 99);
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
