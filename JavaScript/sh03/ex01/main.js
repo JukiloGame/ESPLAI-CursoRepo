a6239cb375817702ac4e97257fd20ddcfa4e872f
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-10-16 15:22:38 +0200

    chore(ex01): Agregar comentarios explicando ejercicio

--- JavaScript/sh03/ex01/main.js
+++ JavaScript/sh03/ex01/main.js
@@ -1,13 +1,13 @@
 const a = [3,1,2];
 
 // Completa con comentarios:
-a.sort();
-const b = a.slice(0, 2);
-const c = b.toReversed();
-const d = a.with(0, 99);
+a.sort(); // Mutable, ya que reordena el array modificando el original
+const b = a.slice(0, 2); // Inmutable, Extrae datos, sin destruir el original
+const c = b.toReversed(); // Inmutable, versión no destructiva de reverse(), copiando los datos
+const d = a.with(0, 99); // Inmutable, copia el array y reemplaza el dato
 
 
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
+console.log("Reordenar array --> " + a);
+console.log("Extraer datos de index 0 y 2 --> " + b);
+console.log("Reordenar en reverso --> " + c);
+console.log("Remplazar dato de indice 0 --> " +d);
