0e7999ab0767906193b0c283938957a18d0cabac
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-10-24 19:05:30 +0200

    feat(ex01): Ejecutar Saludo

--- React/sh01-react-intro/src/App.tsx
+++ React/sh01-react-intro/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Greet from './ex01/Saludo'
 
 export default function App() {
 return (
 <main>
 <h1>Introducción a React</h1>
-
+<Greet name="Luis"/>
 {/* <Composicion /> */}
 {/* <Usuario /> */}
 {/* <PanelLogin /> */}
