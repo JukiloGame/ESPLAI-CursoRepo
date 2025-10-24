1044d41c4ebca9bea903e71f31a97b44d55b880a
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-10-24 18:40:18 +0200

    chore(init): Subir app.tsx

---
+++ React/sh01-react-intro/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+export default function App() {
+return (
+<main>
+<h1>Introducción a React</h1>
+
+{/* <Composicion /> */}
+{/* <Usuario /> */}
+{/* <PanelLogin /> */}
+{/* <Listas /> */}
+{/* <Desafio /> */}
+</main>
+);
+}
