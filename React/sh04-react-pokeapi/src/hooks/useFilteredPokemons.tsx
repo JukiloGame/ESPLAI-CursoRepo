d81d7361d6213f9c5828a031737adcc1d6258383
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-10-29 19:06:57 +0100

    feat(hooks): Crear hook para filtrado de pokemons

---
+++ React/sh04-react-pokeapi/src/hooks/useFilteredPokemons.tsx
@@ -0,0 +1,14 @@
+import { useState, useMemo } from "react";
+import type { Pokemon, Page } from '../types'
+
+export function useFilteredPokemons(pokemons: Pokemon[]) {
+	const [filterText, setFilterText] = useState("");
+
+	const filtered = useMemo(() => {
+		return pokemons.filter( p => 
+		p.name.toLowerCase().includes(filterText.toLowerCase())
+		);
+	}, [pokemons, filterText]);
+
+	return { filtered, filterText, setFilterText}
+}
