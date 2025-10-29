17de86c991bc0349abe51bd2e45d512dece64d28
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-10-29 15:47:50 +0100

    feat(pokeapi): Migrar HTML al Doom, fase temprana

--- React/sh04-react-pokeapi/src/pages/PokeDashboard.tsx
+++ React/sh04-react-pokeapi/src/pages/PokeDashboard.tsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
-import './style.css'
+import PokeCard from '../components/PokeCard'
+import '../style.css'
+
 function PokeDashboard() {
   return (
     <>
-    
+	<section className="pokemonDashboard"> 
+	<div className="pokemonDashboard__filter">
+			<input id="pokemonFilter" type="text" placeholder="Filtrar pokemons por nombre..."/>
+	</div>
+	<PokeCard id={1} name="Bulbasaur" types={["Grass", "Poison"]} img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+	<PokeCard id={1} name="Bulbasaur" types={["Grass", "Poison"]} img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+	<PokeCard id={1} name="Bulbasaur" types={["Grass", "Poison"]} img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+	<PokeCard id={1} name="Bulbasaur" types={["Grass", "Poison"]} img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+	<PokeCard id={1} name="Bulbasaur" types={["Grass", "Poison"]} img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+	<PokeCard id={1} name="Bulbasaur" types={["Grass", "Poison"]} img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+	<PokeCard id={1} name="Bulbasaur" types={["Grass", "Poison"]} img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+	<PokeCard id={1} name="Bulbasaur" types={["Grass", "Poison"]} img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+	</section>
     </>
   )
 }
