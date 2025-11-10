faca02d96d5068b3987e06d674db711c9842b49d
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-11-10 19:11:30 +0100

    feat(j.sh02) Añadir PromedioArray ex03

---
+++ Java/sheet02/ex03/PromedioArray.java
@@ -0,0 +1,16 @@
+package Java.sheet02.ex03;
+
+public class PromedioArray {
+	static int[] numbers = {4, 8, 6, 2};
+
+	public static void main(String[] var0) {
+		System.out.println(calcularPromedio(numbers));
+	}
+	public static double calcularPromedio(int[] numeros) {
+		double total = 0;
+		for (int number: numeros) {
+			total += number;
+		}
+		return total / numeros.length;
+	}
+}
