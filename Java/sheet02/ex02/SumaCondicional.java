b41ad6cdd097b1a680b3127de90d47c2738454e5
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-11-10 19:03:54 +0100

    feat(j.sh02) Añadir SumaCondicional ex02

---
+++ Java/sheet02/ex02/SumaCondicional.java
@@ -0,0 +1,16 @@
+package Java.sheet02.ex02;
+
+public class SumaCondicional {
+	public static void main(String[] var0) {
+		sumarSiPositivo(1, 3);
+		sumarSiPositivo(-2, 4);
+	}
+
+	public static void sumarSiPositivo(int num1, int num2) {
+		int total = 0;
+		if (num1 > 0 && num2 > 0) {
+			total = num1 + num2;
+		}
+		System.out.println(total);
+	}
+}
