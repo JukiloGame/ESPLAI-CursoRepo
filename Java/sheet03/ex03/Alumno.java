546ccb3be8c82e059671d6a47cf8072b8458cbad
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-11-12 18:42:51 +0100

    doc(j.sh03) Explicar como funciona el constructor por defecto de Java

---
+++ Java/sheet03/ex03/Alumno.java
@@ -0,0 +1,12 @@
+package sheet03.ex03;
+
+public class Alumno {
+	private String name;
+	private int age;
+
+	public Alumno () {}
+	public Alumno(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
