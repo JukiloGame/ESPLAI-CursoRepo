6ecc63415fd6cfa07064070f9554b066401f477a
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-11-12 19:03:55 +0100

    feat(ShoppingCart) Añadir lista no expuesta

--- Java/sheet03/ex04/ShoppingCart.java
+++ Java/sheet03/ex04/ShoppingCart.java
@@ -1,10 +1,16 @@
 package sheet03.ex04;
 import sheet03.ex01_02.Product;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ShoppingCart {
-	public class CartLine {
+	private class CartLine {
 		Product product;
 		int units;
 		double subtotal() { return product.GetPrice() * units; }
 	}
+
+	private List<CartLine> cartLines = new ArrayList<>();
+
+	
 }
