aedd5f269cd799b3dd3250945520fb674a459f12
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-11-12 18:50:20 +0100

    feat(j.sh03) Añadir carrito de compra sin subtotal()

---
+++ Java/sheet03/ex04/ShoppingCart.java
@@ -0,0 +1,10 @@
+package sheet03.ex04;
+import sheet03.ex01_02.producto;
+
+public class ShoppingCart {
+	public class LineaCarrito {
+		producto product;
+		int units;
+		// double subtotal() { return product.precio * units; }
+	}
+}
