def61c24d591a8bd9514c2103ba8281fcfeba673
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-11-14 15:58:26 +0100

    reaft(ShoppingCart) añadir construct y refactorizar getProductInfo con getResume

--- Java/sheet03/ex04/ShoppingCart.java
+++ Java/sheet03/ex04/ShoppingCart.java
@@ -6,40 +6,49 @@ import java.util.ArrayList;
 public class ShoppingCart {
 	public static void main(String[] args) {
 		addProduct(new Product("Gambas", 11.59), 50);
-		addProduct(new Product("Fanta", 42), 20);
+		addProduct(new Product("Fanta", 1.59), 20);
 		addProduct(new Product("Gambas", 11.59), 30);
 
-		for (CartLine cartLine : cartLines) {	
-			cartLine.getProductInfo();
-		}
+		getResume();
+
 	}
 	private static class CartLine {
 		Product product;
 		int units;
 		double subtotal() { return product.getPrice() * units; }
 
-		public void getProductInfo() {
-			System.out.printf("Nombre: %s || Unidades %d || Subtotal: %.2f \n", product.getName(), units, subtotal());
-			// System.out.printf(product.GetName() + " || " + units + " || "  +  " || " + subtotal());
+		public CartLine(Product product, int units) {
+			this.product = product;
+			this.units = units;
+		}
+
+		public String getProductInfo() {
+			return String.format("Nombre: %s || Unidades %d || Subtotal: %.2f euros", product.getName(), units, subtotal());
 		}
 	}
 
 	private static List<CartLine> cartLines = new ArrayList<>();
 
-	public static void addProduct(Product p, int units) {
-		if (p == null || units <= 0 || p.getPrice() < 0) return;
+	public static void getResume() {
+		int total = 0;
+		for (CartLine cartLine : cartLines) {	
+			System.out.println(cartLine.getProductInfo());
+			total += cartLine.subtotal();
+		}
+		System.out.println("\n Precio total: " + total + " euros");
+		
+	}
+
+	public static boolean addProduct(Product p, int units) {
+		if (p == null || units <= 0 || p.getPrice() < 0) return false;
 		for (int i = 0; i < cartLines.size(); i++) {
 			if (cartLines.get(i).product.getName().equals(p.getName())) {
-				System.out.println("Unidades mergeadas: ");
 				cartLines.get(i).units += units;
-				return;
+				return true;
 			}
 		}
-		CartLine line = new CartLine();
-		line.product = p;
-		line.units = units;
+		CartLine line = new CartLine(p, units);
 		line.subtotal();
-		cartLines.add(line);
-		System.out.println("Línea creada ");
+		return cartLines.add(line);
 	}
 }
