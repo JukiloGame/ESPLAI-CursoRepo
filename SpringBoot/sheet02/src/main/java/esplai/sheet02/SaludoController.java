bd69770e40a5305c2756093780a0559db04d9963
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-11-20 19:14:18 +0100

    feat(SpringBoot.sh2) Añadir saludo ex01

---
+++ SpringBoot/sheet02/src/main/java/esplai/sheet02/SaludoController.java
@@ -0,0 +1,15 @@
+package esplai.sheet02;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/saludo")
+public class SaludoController {
+
+    @GetMapping("/hola")
+    public String saludo() {
+        return "Servidor operativo. Bienvenido a la API.";
+    }
+}
